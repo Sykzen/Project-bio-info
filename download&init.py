e4062e552ab45bc36fe53d75fb4a523181a3cd64
Sykzen <55047001+Sykzen@users.noreply.github.com>
2022-01-31 14:56:06 +0100

    Add files via upload

--- download&init.py
+++ download&init.py
@@ -1,12 +1,23 @@
-<<<<<<< HEAD
 import json
 import os
 import hashlib
+import urllib.request
 f=open("md5.json")
+url = 'https://pypi.org/project/wget/'
+
 d=json.load(f)
+
 class DownloadAndInit:
-    def __init__(self):
-        pass    
+    def __init__(self,urldata,urlgenome):
+        self.urlData = urldata
+        self.urlGenome=urlgenome
+           
+    def downloadData(self):
+        requests.get(self.url)
+        urllib.request.urlretrieve(self.urlData)
+    def downloadGenomeReference(urlgenome):
+        self.ulrGenome
+        urllib.request.urlretrieve(self.urlGenome)
     def checkmd5():
         for i,j in zip(os.listdir("data"),d):
             for e,k in enumerate(os.listdir("data/"+j["run_accession"])):
@@ -18,22 +29,7 @@ class DownloadAndInit:
                 
         return True
 
-=======
-import json
-import os
-import hashlib
-f=open("md5.json")
-d=json.load(f)
 
-def checkmd5():
-    for i,j in zip(os.listdir("data"),d):
-        for e,k in enumerate(os.listdir("data/"+j["run_accession"])):
-            original_md5=j["fastq_md5"].split(";")[e]
-            with open("data/"+str(i)+"/"+str(k),"rb") as f:
-                bytes = f.read()
-                if hashlib.md5(bytes).hexdigest()!=original_md5:
-                    return False
-                
-    return True
-
->>>>>>> 5e60873f81534ecb119ede952b9dd847ad824f83
+etude=DownloadAndInit("https://www.ebi.ac.uk/ena/browser/api/xml/PRJEB24932?download=true").downloadData()
+etude.downloadGenomeReference("")
+etude.checkmd5()
