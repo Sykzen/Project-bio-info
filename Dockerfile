c7ca628d0488c9cda40a58ea6dab257ab95477ff
sykzen <sykzen@hotmail.com>
2022-02-13 17:03:38 +0100

    refactor : init

--- Dockerfile
+++ Dockerfile
@@ -6,19 +6,33 @@ RUN sudo apt-get -y install git
 RUN sudo apt-get -y install python3
 RUN sudo apt-get install wget
 RUN mkdir /home/Project
-RUN git clone https://github.com/Sykzen/Project-bio-info.git /home/Project
+RUN sudo apt-get -y install openjdk-8-jdk
 WORKDIR /home/Project
+RUN git clone https://github.com/Sykzen/Project-bio-info.git /home/Project
+
 #cd /home/Project/Project-bio-info
 
 #RUN cd Project-bio-info
 RUN git clone https://github.com/lh3/bwa.git
-EXPOSE 5000
+RUN cd bwa; make
+#Download bedtools , sammtools 1.9 ,BCFTools , vcftools
+RUN wget https://github.com/samtools/samtools/releases/download/1.9/samtools-1.9.tar.bz2
+RUN wget https://github.com/samtools/bcftools/releases/download/1.9/bcftools-1.9.tar.bz2
+RUN sudo apt-get install -y vcftools
+RUN sudo apt install bedtools
+RUN sudo apt -y -8 -37 install picard-tools
 
+#Dezip BCFTools,samtools
+RUN tar -vxjf bcftools-1.9.tar.bz2
+#RUN cd bcftools;make
+RUN tar -vxjf samtools-1.9.tar.bz2
+#RUN cd samtools;make
+#Donwload GATK4.2.5.0
+RUN wget https://github.com/broadinstitute/gatk/releases/download/4.2.5.0/gatk-4.2.5.0.zip
+EXPOSE 5000
 #CMD python3 init.py
 
 
 #RUN exec bash
-#RUN conda activate ProjectBio
-#RUN mkdir /home/ProjectBioInfo && cd $_
 
 
--- init.py
+++ init.py
@@ -2,6 +2,10 @@ import json
 import os
 import hashlib
 import urllib.request
+import tarfile
+import gzip
+import shutil
+
 f=json.load(open("filereport.json"))
 
 #d=json.load(f)
@@ -9,14 +13,28 @@ f=json.load(open("filereport.json"))
 class Init:
     def __init__(self):
         pass
-    def downloadData(self,data):
+    def downloadData(self,data,lim):
         link=[i["fastq_ftp"].split(";")[0]  for i in data if len(i["fastq_ftp"].split(";"))==1] + [i["fastq_ftp"].split(";")[1]  for i in data if len(i["fastq_ftp"].split(";"))==2]
-        for i in link:
+        for i in link[:lim]:
             cmd="wget "+ i
             os.system(cmd)            
     def downloadDataX(self,link):
         FinalUrl="wget "+link
+        print(FinalUrl)
         os.system(FinalUrl)
+    def dezipeTgzX(self,link):
+        tar=tarfile.open(link,"r:gz")
+        tar.extractall()
+        tar.close
+    def dezipeGzX(self,link):
+        a = gzip.open(link, 'rb')
+        file_content = a.read()
+        
+        with open("S288C_reference_sequence_R64-3-1_20210421.fsa", "wb") as out:
+            out.truncate(1024 * 1024  )
+            out.write(file_content)
+            out.close()
+        a.close()
     def downloadGenomeReference(urlgenome):
         self.ulrGenome
         urllib.request.urlretrieve(self.urlGenome)
@@ -33,5 +51,13 @@ class Init:
 
 
 t=Init()
-#t.downloadData(f)
+#download full 26 data from PROJ
+t.downloadData(f,2)
+#check the MD5
+#b t.checkmd5()
+#download Genome Reference
 t.downloadX("http://sgd-archive.yeastgenome.org/sequence/S288C_reference/genome_releases/S288C_reference_genome_Current_Release.tgz")
+#dezip genome reference Racine
+t.dezipeTgzX("gn.tgz")
+#dezip genome reference utulisé
+t.dezipeGzX("S288C_reference_genome_R64-3-1_20210421/S288C_reference_sequence_R64-3-1_20210421.fsa.gz")
